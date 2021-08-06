62bde8193070f7592abb8ec6f112576e1ae50e08
happynation <vovatran1993@gmail.com>
2021-08-06 15:37:58 +0000

    test

--- script2.sh
+++ script2.sh
@@ -2,13 +2,15 @@
 # Ask the user for application  name
 echo Hello, what is application name?
 read varname
-value=`cat dod.txt`
+#value=`cat dod.txt`
 
 if [[ $varname == logs ]]
 then
-  ssh $value -- "ps -ef | grep tomcat"
+   for i in $(cat dod.txt) ; do echo "*** Server ${i} ***" &&  ssh $i 'tail $(/bin/ls -1t /tmp/test/test* | /bin/sed q)' ; done
 elif [[ $varname == logs2 ]]
 then
   echo "Hello World2"
 fi
 echo It\'s nice to meet you $varname
+
+
